ac81f24c988400dce84b748fddf7c7bcdb460c93
Rajat Sharma <rsharma.2006@outlook.com>
2025-12-20 00:28:47 -0800

    Add test, validation, and training datasets
    Added testData.csv and validationData.csv for model evaluation and validation. Renamed symptomData.csv to trainingData.csv to clarify its purpose. Updated main.py and modelTrain.py to support new data files and workflow.

--- src/modelTrain.py
+++ src/modelTrain.py
@@ -4,40 +4,42 @@
 # Description: Trains the machine learning model using the symptom dataset.
 
 
-# Import the pandas library for working with the data, and DecisionTreeClassifier from sklearn for building the model.
+# Import the pandas library for working with the data, and RandomForestClassifier from sklearn for building the model.
 # Additionally, import train_test_split for splitting the dataset and accuracy_score, classification_report for evaluating the model.
 import pandas
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 
-def train_classifier():
-    df = pandas.read_csv("data/symptomData.csv")
+def load_data(fp):
+    inFile = pandas.read_csv(fp)
+    X_Var = inFile.drop("diagnosis", axis=1)
+    Y_Var = inFile["diagnosis"]
+    return X_Var, Y_Var
     # Read the dataset from the csv file, utilizing pandas and converting to dataframe.
-
-    X_Var = df.drop("diagnosis", axis=1)
     # Since "diagnosis" is the target variable, we drop it from the feature set.
-    Y_Var = df["diagnosis"]
     # The target variable is stored in Y_Var.
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X_Var, Y_Var, test_size = 0.2, random_state = 42)
-    # Split the dataset into training and testing sets, with 20% of the data reserved for testing, and the 42 random state for reproducibility.
+def train_classifier():
+    X_training, Y_training = load_data("data/trainingData.csv")
+    X_validation, Y_validation = load_data("data/validationData.csv")
+    X_testing, Y_testing = load_data("data/testData.csv")
+    # Data split into training, validation, and testing sets.
 
-    DT_Model = RandomForestClassifier(n_estimators = 200, random_state = 42, max_depth = 8)
+    RF_Model = RandomForestClassifier(n_estimators = 200, random_state = 42, max_depth = 8, class_weight='balanced')
     # Initialize the RandomForestClassifier with 200 trees, a random state of 42, and a maximum depth of 8.
     # A random state of 42 ensures reproducibility of results.
 
-    DT_Model.fit(X_train, Y_train)
+    RF_Model.fit(X_training, Y_training)
     # Fit the model on the training data. Fitting represents the process of learning from the data.
 
-    Y_Pred = DT_Model.predict(X_test)
-    # Make predictions on the test set. This generates predicted labels for the test data.
+    validation_prediction = RF_Model.predict(X_validation)
+    print ("Validation Set Accuracy:", accuracy_score(Y_validation, validation_prediction))
 
-    accuracy = accuracy_score(Y_test, Y_Pred)
-    # Calculate the accuracy of the model by comparing the predicted labels with the actual labels from the test set.
 
-    print("Model Accuracy:", accuracy)
-    print("Classification Report:\n", classification_report(Y_test, Y_Pred))
+    test_prediction = RF_Model.predict(X_testing)
+    print ("Test Set Accuracy:", accuracy_score(Y_testing, test_prediction))
+    print("\nClassification Report:\n", classification_report(Y_testing, test_prediction))
+    # Make predictions on the validation and testing sets, then print accuracy scores and a detailed classification.
 
-    return DT_Model, list(X_Var.columns)
+    return RF_Model, list(X_training.columns)
     # Return the trained model and the list of feature names for future use.
